acde254abfd76b82c2a2f2bd67c3aa9956ce6cf7
Pieter Hordijk <info@pieterhordijk.com>
2020-09-09 04:55:19 +0300

    Removed DI dependency

--- server.php
+++ server.php
@@ -3,62 +3,43 @@
 use Amp\Loop;
 use Amp\Promise;
 use Amp\Websocket\Server\Websocket;
-use Amp\Http\Client\HttpClient;
 use Amp\Http\Client\HttpClientBuilder;
 use Amp\Http\Server\HttpServer;
 use Amp\Http\Server\Router;
 use Amp\Http\Server\StaticContent\DocumentRoot;
 use Amp\Log\ConsoleFormatter;
 use Amp\Socket\Server;
-use Auryn\Injector;
-use ekinhbayar\GitAmp\Github\Credentials;
+use ekinhbayar\GitAmp\Event\Factory as EventFactory;
 use ekinhbayar\GitAmp\Http\Origin;
 use ekinhbayar\GitAmp\Provider\GitHub;
-use ekinhbayar\GitAmp\Provider\Listener;
+use ekinhbayar\GitAmp\Response\Factory as EventCollectionFactory;
 use ekinhbayar\GitAmp\Websocket\Handler;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
-use Psr\Log\LoggerInterface;
 use function Amp\ByteStream\getStdout;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $configuration = require_once __DIR__ . '/config.php';
 
-$injector = new Injector;
+$logHandler = new StreamHandler(getStdout()->getResource(), $configuration['logLevel']);
+$logHandler->setFormatter(new ConsoleFormatter());
 
-$injector->delegate(HttpClient::class, function () {
-    return HttpClientBuilder::buildDefault();
-});
+$logger = new Logger('gitamp');
+$logger->pushHandler($logHandler);
 
-$injector->share(HttpClient::class);
+$gitHubListener = new GitHub(
+    HttpClientBuilder::buildDefault(),
+    $configuration['github'],
+    new EventCollectionFactory(new EventFactory(), $logger),
+    $logger,
+);
 
-$injector->alias(Listener::class, GitHub::class);
-
-$injector->alias(Credentials::class, get_class($configuration['github']));
-
-$injector->share($configuration['github']);
-
-$injector->alias(LoggerInterface::class, Logger::class);
-$injector->share(Logger::class);
-
-$injector->delegate(Logger::class, function () use ($configuration) {
-    $logHandler = new StreamHandler(getStdout()->getResource(), $configuration['logLevel']);
-    $logHandler->setFormatter(new ConsoleFormatter());
-
-    $logger = new Logger('gitamp');
-    $logger->pushHandler($logHandler);
-
-    return $logger;
-});
-
-$logger = $injector->make(Logger::class);
-
-$injector->define(Handler::class, [
-    ':origin' => (new Origin($configuration))->get(),
-]);
-
-$clientHandler = $injector->make(Handler::class);
+$clientHandler = new Handler(
+    (new Origin($configuration))->get(),
+    $gitHubListener,
+    $logger,
+);
 
 $websocket = new Websocket($clientHandler);
 
