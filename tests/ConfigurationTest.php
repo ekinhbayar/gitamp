0194f9dda626b977c9332aa4f11583b6e2c2e243
Pieter Hordijk <info@pieterhordijk.com>
2020-09-14 15:23:59 +0300

    Create configuration class

---
+++ tests/ConfigurationTest.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+
+namespace ekinhbayar\GitAmpTests;
+
+use ekinhbayar\GitAmp\Configuration;
+use ekinhbayar\GitAmp\Github\Token;
+use ekinhbayar\GitAmp\ServerAddress;
+use League\Uri\Uri;
+use Monolog\Logger;
+use PHPUnit\Framework\TestCase;
+
+class ConfigurationTest extends TestCase
+{
+    private Configuration $configuration;
+
+    public function setUp(): void
+    {
+        $this->configuration = new Configuration(new Token('12345'));
+    }
+
+    public function testDefaultLogLevelAccessors(): void
+    {
+        $this->assertSame(Logger::INFO, $this->configuration->getLogLevel());
+    }
+
+    public function testLogLevelAccessors(): void
+    {
+        $this->configuration->setLogLevel(Logger::ALERT);
+
+        $this->assertSame(Logger::ALERT, $this->configuration->getLogLevel());
+    }
+
+    public function testWebsocketAddressesAccessors(): void
+    {
+        $this->configuration->addWebsocketAddress(Uri::createFromString('http://example.com'));
+        $this->configuration->addWebsocketAddress(Uri::createFromString('http://example.com:1337'));
+
+        $this->assertTrue($this->configuration->websocketAddressExists('http://example.com'));
+        $this->assertTrue($this->configuration->websocketAddressExists('http://example.com:1337'));
+
+        $this->assertFalse($this->configuration->websocketAddressExists('http://xample.com'));
+    }
+
+    public function testBindAccessors(): void
+    {
+        $this->configuration->bind(new ServerAddress('127.0.0.1', 80));
+        $this->configuration->bind(new ServerAddress('127.0.0.1', 8080));
+
+        $this->assertCount(2, $this->configuration->getServerAddresses());
+    }
+
+    public function testGetGithubToken(): void
+    {
+        $this->assertSame('12345', $this->configuration->getGithubToken()->getAuthenticationString());
+    }
+}
