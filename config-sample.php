e4ce98f7a8268f768110b7d3bd0ad69595835706
Pieter Hordijk <info@pieterhordijk.com>
2020-09-14 15:26:07 +0300

    Updated sample config to use the configuration class

--- config-sample.php
+++ config-sample.php
@@ -1,35 +1,15 @@
-<?php
+<?php declare(strict_types=1);
 
-use Monolog\Logger;
+namespace ekinhbayar\GitAmp;
+
+use League\Uri\Uri;
 use ekinhbayar\GitAmp\Github\Token;
 
-return [
-    'logLevel' => Logger::INFO,
-    'hostname' => 'localhost',
-    /**
-     * Assign the IP and port on which to listen.
-     * Use :
-        "0.0.0.0" for all IPv4 interfaces
-        "::"      for all IPv6 interfaces
-        "*"       for all IPv4 & IPv6
-     *
-     */
-    'expose' => [
-        'ip' => '*',
-        'port' => 1337
-    ],
-    /**
-     * Uncomment to use ssl
-    'ssl' => [
-        'ip' => '*',
-        'port' => 443,
-        'certificate' => '/path/to/certificate.pem',
-        'key' => '/path/to/key.pem',
-    ],
-     */
-    /**
-     * Use a personal access token for authentication
-     * https://github.com/settings/tokens
-     */
-    'github' => new Token('your-token'),
-];
+/**
+ * Use a personal access token for authentication
+ * https://github.com/settings/tokens
+ */
+return (new Configuration(new Token('123456')))
+    ->addWebsocketAddress(Uri::createFromString('https://gitamp.audio'))
+    ->bind(new ServerAddress('127.0.0.1', 1337))
+;
