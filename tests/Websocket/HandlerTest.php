87f85aaf868b4dad1fc991697243ec447c643014
Pieter Hordijk <info@pieterhordijk.com>
2020-09-14 15:27:38 +0300

    Fixed and refactored handler tests
    Tests do no have 25 second timeouts anymore

--- tests/Websocket/HandlerTest.php
+++ tests/Websocket/HandlerTest.php
@@ -9,17 +9,19 @@ use Amp\Http\Server\HttpServer;
 use Amp\Http\Server\Request;
 use Amp\Http\Server\RequestHandler\CallableRequestHandler;
 use Amp\Http\Server\Response;
-use Amp\Http\Server\Router;
 use Amp\Http\Status;
+use Amp\Loop;
 use Amp\Socket\Server;
+use Amp\Success;
 use Amp\Websocket\Client as WebsocketClient;
 use Amp\Websocket\Server\Gateway;
-use Amp\Loop;
-use Amp\Success;
-use Amp\Websocket\Server\Websocket;
-use ekinhbayar\GitAmp\Provider\GitHub;
+use ekinhbayar\GitAmp\Configuration;
+use ekinhbayar\GitAmp\Github\Token;
+use ekinhbayar\GitAmp\Provider\Listener;
 use ekinhbayar\GitAmp\Response\Results;
+use ekinhbayar\GitAmp\ServerAddress;
 use ekinhbayar\GitAmp\Websocket\Handler;
+use League\Uri\Uri;
 use Monolog\Logger;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\UriInterface as PsrUri;
@@ -27,22 +29,28 @@ use function Amp\Promise\wait;
 
 class HandlerTest extends TestCase
 {
-    private string $origin;
+    private Listener $listener;
 
-    private $gitamp;
+    private Gateway $gateway;
 
-    private $gateway;
-
-    private $logger;
+    private Logger $logger;
 
     private HttpServer $httpServer;
 
+    private Handler $handler;
+
     public function setUp(): void
     {
-        $this->origin     = 'https://gitamp.audio';
-        $this->gitamp     = $this->createMock(GitHub::class);
-        $this->gateway    = $this->createMock(Gateway::class);
-        $this->logger     = $this->createMock(Logger::class);
+        $this->listener = $this->createMock(Listener::class);
+
+        $configuration = (new Configuration(new Token('12345')))
+            ->addWebsocketAddress(Uri::createFromString('https://gitamp.audio'))
+            ->bind(new ServerAddress('127.0.0.1', 1337))
+        ;
+
+        $this->logger  = $this->createMock(Logger::class);
+        $this->gateway = $this->createMock(Gateway::class);
+
         $this->httpServer = new HttpServer(
             [Server::listen("tcp://127.0.0.1:0")],
             new CallableRequestHandler(function () {
@@ -51,19 +59,18 @@ class HandlerTest extends TestCase
                 return new Response(Status::NO_CONTENT);
             }
         ), $this->logger);
+
+        $this->handler = new Handler($this->listener, $configuration, $this->logger);
     }
 
     public function testHandleHandshakeReturnsForbiddenOnInvalidOrigin(): void
     {
-        $handler = new Handler($this->origin, $this->gitamp, $this->logger);
-
-        $client = $this->createMock(Client::class);
-
-        $uri = $this->createMock(PsrUri::class);
-
-        $request = new Request($client, 'GET', $uri, ['origin' => 'https://notgitamp.audio']);
-
-        $response = new Response(403);
+        $request = new Request(
+            $this->createMock(Client::class),
+            'GET',
+            $this->createMock(PsrUri::class),
+            ['origin' => 'https://notgitamp.audio'],
+        );
 
         $this->gateway
             ->expects($this->once())
@@ -71,24 +78,21 @@ class HandlerTest extends TestCase
             ->willReturn(new DefaultErrorHandler())
         ;
 
-        $handler->handleHandshake($this->gateway, $request, $response);
+        $this->handler->handleHandshake($this->gateway, $request, new Response());
     }
 
     public function testHandleHandshakeReturnsSuccessfulResponseWhenOriginsMatch(): void
     {
-        $handler = new Handler($this->origin, $this->gitamp, $this->logger);
+        $request = new Request(
+            $this->createMock(Client::class),
+            'GET',
+            $this->createMock(PsrUri::class),
+            ['origin' => 'https://gitamp.audio'],
+        );
 
-        $client = $this->createMock(Client::class);
+        $response = wait($this->handler->handleHandshake($this->gateway, $request, new Response()));
 
-        $uri = $this->createMock(PsrUri::class);
-
-        $request = new Request($client, 'GET', $uri, ['origin' => $this->origin]);
-
-        $response = new Response(200);
-
-        $result = wait($handler->handleHandshake($this->gateway, $request, $response));
-
-        $this->assertSame($response->getStatus(), $result->getStatus());
+        $this->assertSame(200, $response->getStatus());
     }
 
     public function testOnStartEmitsWithoutEvents(): void
@@ -98,17 +102,17 @@ class HandlerTest extends TestCase
         $results
             ->expects($this->once())
             ->method('hasEvents')
-            ->will($this->returnValue(false));
+            ->will($this->returnValue(false))
+        ;
 
-        $this->gitamp
+        $this->listener
             ->expects($this->once())
             ->method('listen')
-            ->will($this->returnValue(new Success($results)));
+            ->will($this->returnValue(new Success($results)))
+        ;
 
-        $handler = new Handler($this->origin, $this->gitamp, $this->logger);
-
-        Loop::run(function () use ($handler) {
-            $handler->onStart($this->httpServer, $this->gateway);
+        Loop::run(function () {
+            yield $this->handler->onStart($this->httpServer, $this->gateway);
 
             Loop::stop();
         });
@@ -119,38 +123,40 @@ class HandlerTest extends TestCase
         $results = $this->createMock(Results::class);
 
         $results
-            ->expects($this->exactly(2))
+            ->expects($this->once())
             ->method('hasEvents')
-            ->will($this->returnValue(true));
+            ->will($this->returnValue(true))
+        ;
 
-        $this->gitamp
-            ->expects($this->exactly(2))
+        $this->listener
+            ->expects($this->once())
             ->method('listen')
-            ->will($this->returnValue(new Success($results)));
+            ->will($this->returnValue(new Success($results)))
+        ;
 
         $this->gateway
-            ->expects($this->exactly(2))
-            ->method('broadcast');
+            ->expects($this->once())
+            ->method('broadcast')
+        ;
 
-        $handler = new Handler($this->origin, $this->gitamp, $this->logger);
+        Loop::run(function () {
+            yield $this->handler->onStart($this->httpServer, $this->gateway);
 
-        Loop::run(function () use ($handler) {
-            $handler->onStart($this->httpServer, $this->gateway);
-
-            Loop::delay(25000, "Amp\\Loop::stop");
+            Loop::stop();
         });
     }
 
     public function testHandleClientWithoutExistingEvents(): void
     {
         $results = $this->createMock(Results::class);
+
         $results
             ->expects($this->once())
             ->method('hasEvents')
             ->willReturn(false)
         ;
 
-        $this->gitamp
+        $this->listener
             ->expects($this->once())
             ->method('listen')
             ->willReturn(new Success($results))
@@ -160,8 +166,6 @@ class HandlerTest extends TestCase
             ->expects($this->once())
             ->method('broadcast')
         ;
-
-        $handler = new Handler($this->origin, $this->gitamp, $this->logger);
 
         $websocketClient = $this->createMock(WebsocketClient::class);
 
@@ -170,42 +174,44 @@ class HandlerTest extends TestCase
             ->method('send')
         ;
 
-        $client = $this->createMock(Client::class);
+        $request = new Request(
+            $this->createMock(Client::class),
+            'GET',
+            $this->createMock(PsrUri::class),
+            ['origin' => 'https://gitamp.audio'],
+        );
 
-        $uri = $this->createMock(PsrUri::class);
+        Loop::run(function () use ($websocketClient, $request) {
+            Loop::defer(function () {
+                Loop::stop();
+            });
 
-        $request = new Request($client, 'GET', $uri, ['origin' => $this->origin]);
+            yield $this->handler->onStart($this->httpServer, $this->gateway);
 
-        $response = new Response(200);
-
-        Loop::run(function () use ($handler, $websocketClient, $request, $response) {
-            $handler->onStart($this->httpServer, $this->gateway);
-
-            $handler->handleClient($this->gateway, $websocketClient, $request, $response);
-
-            Loop::stop();
+            yield $this->handler->handleClient($this->gateway, $websocketClient, $request, new Response());
         });
     }
 
     public function testHandleClientWithExistingEvents(): void
     {
-        $this->markTestSkipped('Need to come back to this');
+        //$this->markTestSkipped('Need to come back to this');
 
         $results = $this->createMock(Results::class);
+
         $results
-            ->expects($this->exactly(2))
+            ->expects($this->once())
             ->method('hasEvents')
             ->willReturn(true)
         ;
 
         $results
-            ->expects($this->exactly(2))
+            ->expects($this->once())
             ->method('jsonEncode')
             ->willReturn('{}')
         ;
 
-        $this->gitamp
-            ->expects($this->exactly(2))
+        $this->listener
+            ->expects($this->once())
             ->method('listen')
             ->willReturn(new Success($results))
         ;
@@ -215,36 +221,28 @@ class HandlerTest extends TestCase
             ->method('broadcast')
         ;
 
-        $handler = new Handler($this->origin, $this->gitamp, $this->logger);
-
         $websocketClient = $this->createMock(WebsocketClient::class);
 
-        $websocketClient
-            ->expects($this->once())
-            ->method('send')
-        ;
+        $request = new Request(
+            $this->createMock(Client::class),
+            'GET',
+            $this->createMock(PsrUri::class),
+            ['origin' => 'https://gitamp.audio'],
+        );
 
-        $client = $this->createMock(Client::class);
+        Loop::run(function () use ($websocketClient, $request) {
+            Loop::defer(function () {
+                Loop::stop();
+            });
 
-        $uri = $this->createMock(PsrUri::class);
+            yield $this->handler->onStart($this->httpServer, $this->gateway);
 
-        $request = new Request($client, 'GET', $uri, ['origin' => $this->origin]);
-
-        $response = new Response(200);
-
-        Loop::run(function () use ($handler, $websocketClient, $request, $response) {
-            $handler->onStart($this->httpServer, $this->gateway);
-
-            yield $handler->handleClient($this->gateway, $websocketClient, $request, $response);
-
-            Loop::stop();
+            yield $this->handler->handleClient($this->gateway, $websocketClient, $request, new Response());
         });
     }
 
     public function testOnStopReturnsNothing(): void
     {
-        $handler = new Handler($this->origin, $this->gitamp, $this->logger);
-
-        $this->assertNull(wait($handler->onStop($this->httpServer, $this->gateway)));
+        $this->assertNull(wait($this->handler->onStop($this->httpServer, $this->gateway)));
     }
 }
