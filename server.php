1ca1f29cbf96160eb384b1ca8db3f0fa8ee4f40e
Pieter Hordijk <info@pieterhordijk.com>
2020-09-14 15:24:20 +0300

    Abstracted the server

--- server.php
+++ server.php
@@ -1,62 +1,23 @@
 <?php declare(strict_types=1);
 
+namespace ekinhbayar\GitAmp;
+
 use Amp\Loop;
-use Amp\Promise;
-use Amp\Websocket\Server\Websocket;
-use Amp\Http\Client\HttpClientBuilder;
-use Amp\Http\Server\HttpServer;
-use Amp\Http\Server\Router;
-use Amp\Http\Server\StaticContent\DocumentRoot;
-use Amp\Log\ConsoleFormatter;
-use Amp\Socket\Server;
-use ekinhbayar\GitAmp\Event\Factory as EventFactory;
-use ekinhbayar\GitAmp\Http\Origin;
-use ekinhbayar\GitAmp\Provider\GitHub;
-use ekinhbayar\GitAmp\Response\Factory as EventCollectionFactory;
-use ekinhbayar\GitAmp\Websocket\Handler;
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-use function Amp\ByteStream\getStdout;
+use ekinhbayar\GitAmp\Log\LoggerFactory;
 
 require __DIR__ . '/vendor/autoload.php';
 
+/** @var Configuration $configuration */
 $configuration = require_once __DIR__ . '/config.php';
 
-$logHandler = new StreamHandler(getStdout()->getResource(), $configuration['logLevel']);
-$logHandler->setFormatter(new ConsoleFormatter());
+$logger = (new LoggerFactory())->build($configuration->getLogLevel());
 
-$logger = new Logger('gitamp');
-$logger->pushHandler($logHandler);
-
-$gitHubListener = new GitHub(
-    HttpClientBuilder::buildDefault(),
-    $configuration['github'],
-    new EventCollectionFactory(new EventFactory(), $logger),
-    $logger,
-);
-
-$clientHandler = new Handler(
-    (new Origin($configuration))->get(),
-    $gitHubListener,
-    $logger,
-);
-
-$websocket = new Websocket($clientHandler);
-
-Amp\Loop::setErrorHandler(function (Throwable $e) use ($logger) {
+Loop::setErrorHandler(function (\Throwable $e) use ($logger): void {
     $logger->emergency('GitAmp blew up', ['exception' => $e]);
 });
 
-Loop::run(function () use ($websocket, $configuration, $logger): Promise {
-    $sockets = [
-        Server::listen(sprintf('%s:%d', $configuration['expose']['ip'], $configuration['expose']['port'])),
-    ];
+$server = new Server($logger, $configuration);
 
-    $router = new Router;
-    $router->addRoute('GET', '/ws', $websocket);
-    $router->setFallback(new DocumentRoot(__DIR__ . '/public'));
-
-    $server = new HttpServer($sockets, $router, $logger);
-
-    return $server->start();
+Loop::run(function () use ($server) {
+    yield $server->start();
 });
